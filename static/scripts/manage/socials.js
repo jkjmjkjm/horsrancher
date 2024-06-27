649f329e0dde3479355800ee002f87fabeddf253
Jaime VC <64843349+jkjmjkjm@users.noreply.github.com>
2024-06-27 20:51:38 +0200

    Advancements in social networking for centers

---
+++ static/scripts/manage/socials.js
@@ -0,0 +1,48 @@
+function checkInputs() {
+    const urlPatterns = {
+        facebook: /^https:\/\/(www\.)?facebook.com\/[a-zA-Z0-9(\.\?)?]/,
+        instagram: /^https:\/\/(www\.)?instagram.com\/[a-zA-Z0-9_]+$/,
+        linkedin: /^https:\/\/(www\.)?linkedin.com\/in\/[a-zA-Z0-9-]+$/,
+        pinterest: /^https:\/\/(www\.)?pinterest.com\/[a-zA-Z0-9_]+\/?$/,
+        reddit: /^https:\/\/(www\.)?reddit.com\/user\/[a-zA-Z0-9_]+$/,
+        tiktok: /^https:\/\/(www\.)?tiktok.com\/@?[a-zA-Z0-9_.]+$/,
+        twitter: /^https:\/\/(www\.)?twitter.com\/[a-zA-Z0-9_]+$/,
+        vimeo: /^https:\/\/(www\.)?vimeo.com\/[a-zA-Z0-9]+$/,
+        whatsapp: /^https:\/\/wa.me\/\d+$/,
+        youtube: /^https:\/\/(www\.)?youtube.com\/(user|channel)\/[a-zA-Z0-9_-]+$/,
+    };
+    
+    const phonePattern = /^\+?\d{1,4}[-.\s]?(\(?\d{1,3}?\))?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+
+    const facebookInput = document.getElementById('facebook').value;
+    const instagramInput = document.getElementById('instagram').value;
+    const linkedinInput = document.getElementById('linkedin').value;
+    const pinterestInput = document.getElementById('pinterest').value;
+    const redditInput = document.getElementById('reddit').value;
+    const tiktokInput = document.getElementById('tiktok').value;
+    const twitterInput = document.getElementById('twitter').value;
+    const vimeoInput = document.getElementById('vimeo').value;
+    const youtubeInput = document.getElementById('youtube').value;
+
+    const inputs = [
+        { value: facebookInput, pattern: urlPatterns.facebook, field: 'Facebook' },
+        { value: instagramInput, pattern: urlPatterns.instagram, field: 'Instagram' },
+        { value: linkedinInput, pattern: urlPatterns.linkedin, field: 'LinkedIn' },
+        { value: pinterestInput, pattern: urlPatterns.pinterest, field: 'Pinterest' },
+        { value: redditInput, pattern: urlPatterns.reddit, field: 'Reddit' },
+        { value: tiktokInput, pattern: urlPatterns.tiktok, field: 'TikTok' },
+        { value: twitterInput, pattern: urlPatterns.twitter, field: 'Twitter' },
+        { value: vimeoInput, pattern: urlPatterns.vimeo, field: 'Vimeo' },
+        { value: youtubeInput, pattern: urlPatterns.youtube, field: 'YouTube' },
+    ];
+
+    let isValid = true;
+    
+    inputs.forEach(input => {
+        if (input.value && !input.pattern.test(input.value)) {
+            alert(`${input.field} URL is invalid`);
+            isValid = false;
+        }
+    });
+    return isValid;
+}
